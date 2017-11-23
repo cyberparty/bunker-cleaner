3fd2fff7d929181523c180146776f19c1b9930bf
imj0seph <joejc1999@hotmail.co.uk>
2017-11-23 19:34:59 +0000

    Modified bot for discord.py rewrite

--- main.py
+++ main.py
@@ -5,34 +5,36 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 des = "Someone's gotta clean up those drugs."
 pref = "!"
-client = commands.Bot(description=des, command_prefix=pref)
+client = discord.Client()
+bot = commands.Bot(description=des, command_prefix=pref)
 
-@client.event
+@bot.event
 async def on_ready():
     print("Commander, your favourite cleaner bot is now online!")
-    print("Name:", client.user.name)
-    print("ID:", client.user.id)
-    await client.change_presence(game=discord.Game(name="FDX_GOL"))
+    print("Name:", bot.user.name)
+    print("ID:", bot.user.id)
+    await bot.change_presence(game=discord.Game(name="FDX_GOL"))
+    await bot.get_channel(187319694363983873).send_message("Yeah yeah, I'm here, let me get my mop.")
 
-@client.event
+@bot.event
 async def on_message(message):
     print(message.author, "|", message.channel, ">",message.content)
-    await client.process_commands(message)
+    await bot.process_commands(message)
 
-@client.command(pass_context=True)
+@bot.command()
 async def barney(ctx):
-    await client.say("Barney is your god. All hail the midget dog.")
+    await ctx.send("Barney is your god. All hail the midget dog.")
 
-@client.command(pass_context=True)
+@bot.command()
 async def sin(ctx):
-    await client.say("None of you are free of sin.")
+    await ctx.send("None of you are free of sin.")
 
-@client.command(pass_context=True)
+@bot.command()
 async def cat(ctx):
     factFile = open("catfacts.txt", "r")
     facts = factFile.read().splitlines()
     factFile.close()
-    await client.say(random.choice(facts))
+    await ctx.send(random.choice(facts))
 
-client.run("key")
+bot.run("key-goes-here")
 
